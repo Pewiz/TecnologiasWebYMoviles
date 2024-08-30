f7f21049ef474122a5cd136b0f269a47607ddd0e
David Osorio <dvilla20152025@gmail.com>
2024-08-30 04:29:10 -0400

    Confetti
    Le agregué una funcionalidad de que cada vez que se haga click en algún día feriado, de este va a salir confetti. Para que pueda salir confetti en la posición correcta lo que realicé fue obtener las coordenadas del mouse al momento de hacer click con las propiedades clientX: , clientY de onClick

--- 01-API/src/components/DiaFeriado.jsx
+++ 01-API/src/components/DiaFeriado.jsx
@@ -1,20 +1,39 @@
+import confetti from "canvas-confetti";
 
-const DiaFeriado = ({data, tipoFeriado, getRandomColor}) =>{
-    return(
-        data.map((element) => {
-            return(
-              <article key={element.date} className='contenedorDia' style={{ backgroundColor: getRandomColor()}}>
-                
-                <div className='text'>
-                  <div>📆 {element.date}</div>
-                  <div>🥳 {element.title}</div>
-                  <div>{tipoFeriado(element)}</div>
-                </div>
-                <div className='overlay'></div>
-              </article>
-            )
-          })
-    )
-}
+const DiaFeriado = ({ data, tipoFeriado, getRandomColor }) => {
 
-export default DiaFeriado
+  const ejecutarConfetti = (event) => {
+    const { clientX: x, clientY: y } = event;
+    confetti({
+      particleCount: 100,
+      startVelocity: 30,
+      spread: 360,
+      origin: {
+        x: x / window.innerWidth,  // Normaliza la coordenada X o sea obtiene valores entre 0 y 1 para que los confettis no se salga de la ventana
+        y: y / window.innerHeight, // Normaliza la coordenada Y o sea obtiene valores entre 0 y 1 para que los confettis no se salga de la ventana
+      }
+    });
+  };
+
+  return (
+    data.map((element) => {
+      return (
+        <article
+          onClick={ejecutarConfetti}
+          key={element.date}
+          className="contenedorDia"
+          style={{ backgroundColor: getRandomColor() }}
+        >
+          <div className="text">
+            <div>📆 {element.date}</div>
+            <div>🥳 {element.title}</div>
+            <div>{tipoFeriado(element)}</div>
+          </div>
+          <div className="overlay"></div>
+        </article>
+      );
+    })
+  );
+};
+
+export default DiaFeriado;
